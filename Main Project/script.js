1a4daf1dbc42c71ffada3d5c4a7ffd80c0f77786
Edvin <116942348+e-disha@users.noreply.github.com>
2022-12-21 22:25:36 +0100

    Add files via upload

---
+++ Main Project/script.js
@@ -0,0 +1,15 @@
+// POPUP when click REGJISTROHU
+const toExecute = () =>{
+    window.open("form.html", "mywindow","menubar=1, resizable=1, width=600, height=500");
+}
+
+//BUTONI TE FORMA
+function alertMsg(){
+    alert('Regjistrimi juaj u krye me sukses!');
+    window.close();
+}
+
+//BUTONI TE PYETJET
+function dergoMsg(){
+    alert('Pyetja juaj u dergua me sukses dhe do perpunohet nga ana jone.');
+}
